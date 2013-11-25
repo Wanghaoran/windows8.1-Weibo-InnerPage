e8ab810f0e1988e727797c0ad65b34eceffe9b3a
WangHaoRan <whrlmc@139.com>
2013-11-26 02:35:46 +0800

    完成 第二阶段 页面及 功能

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -12,4 +12,13 @@ class IndexAction extends Action {
     public function part2(){
         $this -> display();
     }
+
+    public function part3(){
+        $id = !empty($_GET['tid']) ? $this -> _get('tid', 'intval') : 1;
+        $result = R('Topic/topic',array($id),'Widget');
+        $this -> assign('result', $result);
+        $list = R('Topic/topic',array($id, true),'Widget');
+        $this -> assign('list', $list);
+        $this -> display();
+    }
 }
