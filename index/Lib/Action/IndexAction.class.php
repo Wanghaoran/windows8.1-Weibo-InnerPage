76d433ef2724764e1a66b5adc9a4a8f52ee50ce7
WangHaoRan <whrlmc@139.com>
2013-12-05 09:30:00 +0800

    将话题切换为第二个

--- index/Lib/Action/IndexAction.class.php
+++ index/Lib/Action/IndexAction.class.php
@@ -2,7 +2,7 @@
 class IndexAction extends Action {
     public function index(){
 //        $id = !empty($_GET['tid']) ? $this -> _get('tid', 'intval') : 1;
-        $id = 1;
+        $id = 2;
         $result = R('Topic/topic',array($id),'Widget');
         $this -> assign('result', $result);
         $list = R('Topic/topic',array($id, true),'Widget');
