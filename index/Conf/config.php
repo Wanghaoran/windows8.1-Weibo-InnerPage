42642b620209e4b54ca60ac7000a16572cd063bb
WangHaoRan <whrlmc@139.com>
2013-12-12 12:30:39 +0800

    第二阶段页面上线

--- index/Conf/config.php
+++ index/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 $common_config = include './Public/config.public.php';
 $app_config = array(
-    //'配置项'=>'配置值'
+    'DEFAULT_ACTION' => 'part2',
 );
 return array_merge($common_config, $app_config);
 
