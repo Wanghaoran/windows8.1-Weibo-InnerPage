5e2ca78d98b331a39c22c485639ed9ad89b53092
WangHaoRan <whrlmc@139.com>
2013-12-31 11:47:22 +0800

    更改文字

--- index/Conf/config.php
+++ index/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 $common_config = include './Public/config.public.php';
 $app_config = array(
-    'DEFAULT_ACTION' => 'part2',
+    'DEFAULT_ACTION' => 'part4',
 );
 return array_merge($common_config, $app_config);
 
